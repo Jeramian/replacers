b867a89daec95b58f3fe2019d76d1e248c923658
jeramian <jeremyleramian@outlook.com>
2016-04-29 11:21:29 -0700

    initia commit

---
+++ main.js
@@ -0,0 +1,33 @@
+var input;
+var sentence;
+
+input = window.prompt('Please enter a sentence');
+if(input.length > 0)
+{
+	sentence = input;
+	sentence.toLowerCase();
+}
+else
+{
+	alert('You did not enter a sentence');
+}
+
+var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+var replacers = ['w', 'q', 'l', 'x', 'p', 'h'];
+
+var indices = [];
+
+for(var i=0; i<sentence.length; i++)
+{
+	if(sentence[i] === 'a')
+	{
+		indices.push(i);
+	}
+}
+
+var list = indices.toString();
+
+if(list.length > 0)
+{
+	
+}
